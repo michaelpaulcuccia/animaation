7b045e80a070bc06dd1e001e83e30d892ec52ed5
Michael Cuccia <michael.cuccia@egplusww.com>
2023-04-03 12:52:23 -0400

    basic app setup

---
+++ client/layout/Container.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: 0 20px;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: white;
+`;
+
+const ComponentWrapper = ({ children, className }) => {
+  return <Container className={className}>{children}</Container>;
+};
+
+export default ComponentWrapper;
